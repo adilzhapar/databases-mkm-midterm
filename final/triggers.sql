51a3505fdc7fee2ab0330e4d0aee264e1d6297b3
Adil Zhapar <adilzhapar2002@gmail.com>
2023-05-09 15:20:48 +0600

    password encryption and 2 trigger

--- final/triggers.sql
+++ final/triggers.sql
@@ -33,8 +33,33 @@ FOR EACH ROW
 EXECUTE FUNCTION update_payment_status();
 
 
+-- 2 instead of crypto, we modify password to be crypted with extension
+
+CREATE EXTENSION pgcrypto;
+
+insert into "user" values (DEFAULT, 'Adil2', 'Zhapar', 'zhapar@gmail.com', 87081234567, 'MALE', '2002-08-13', crypt('adil2002', gen_salt('bf')), default, 1);
 
 
+-- 3
+-- trigger to recalculate product rate after new review
 
+CREATE OR REPLACE FUNCTION recalculate_rate()
+returns trigger as $$
+    declare num_of_reviews INTEGER;
+    DECLARE sum_of_reviews FLOAT;
+    declare final_result float;
+BEGIN
+    num_of_reviews := (SELECT COUNT(review_id) from product_review where product_id = NEW.product_id);
+    sum_of_reviews := (SELECT SUM(rating) from product_review where product_id = NEW.product_id);
+    final_result := round((sum_of_reviews / num_of_reviews)::numeric, 2)::float;
+    update product set rating_score = final_result where product_id=NEW.product_id;
 
+    return NEW;
+end;
+$$ language plpgsql;
+
+create trigger rate_calculator_trigger
+AFTER INSERT OR UPDATE ON product_review
+FOR EACH ROW
+EXECUTE FUNCTION recalculate_rate();
 
